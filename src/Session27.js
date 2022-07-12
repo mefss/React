b1343631807f2a0d25b62ec16ba74c33833b2a18
mefss <mef_ss@yahoo.com>
2022-07-12 12:41:48 +0430

    commit

---
+++ src/Session27.js
@@ -0,0 +1,13 @@
+import './Session27.css'
+
+const Session27 = () => {
+    return (
+        <div className='session27'>
+            <h1>
+                شمارنده ی من:
+            </h1>
+        </div>
+    )
+}
+
+export default Session27;
